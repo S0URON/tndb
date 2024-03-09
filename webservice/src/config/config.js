029b4aa4fe0898edbc905ede1f3144c64e6bdcfc
El Mokni MedAmin <36445054+S0URON@users.noreply.github.com>
2024-03-09 15:22:01 +0100

    Add files via upload

---
+++ webservice/src/config/config.js
@@ -0,0 +1,4 @@
+export default {
+  PORT: 3000 | process.env.PORT,
+  MONGO_URI: "mongodb://localhost:27017",
+};
---
+++ webservice/src/middlewares/upload.js
@@ -0,0 +1,29 @@
+import multer from "multer";
+
+const MulterStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./src/static");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      `${file.originalname.split(".")[0]}.${file.mimetype.split("/")[1]}`
+    );
+  },
+});
+
+export const MulterUpload = multer({
+  storage: MulterStorage,
+  limits: { fieldSize: 25 * 1024 * 1024 },
+}).fields([
+  {
+    name: "coverPicture",
+    maxCount: 1,
+    type: "file",
+  },
+  {
+    name: "profilePicture",
+    maxCount: 1,
+    type: "file",
+  },
+]);
